3dfbe28275719861ac03ef61835694c04994c9db
Andre <andrevitorlopes@gmail.com>
2024-04-14 12:08:45 +0200

    Fix deploy and build

--- platform.wplatform.js
+++ platform.wplatform.js
@@ -2,10 +2,10 @@ exports.Platform = class Platform {
   postSynth(config) {
     config.terraform.backend = {
       s3: {
-        bucket: "terraform-medium-api-notification",
-        key: "book-manager-winglang/state.tfstate",
-      }  
-    }
+        bucket: 'terraform-medium-api-notification',
+        key: 'book-manager-winglang/state.tfstate',
+      },
+    };
     return config;
   }
-}
+};
